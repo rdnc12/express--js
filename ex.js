1b73a9587fadabee25e89e875b56bdbaf3429714
Erdinc <Erdinc@gklfmg.com>
2019-09-04 15:12:44 +0200

    function 2 finished

--- ex.js
+++ ex.js
@@ -5,45 +5,49 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.listen(3000, function () {
     console.log('Server is running with port 3000');
 });
 
 
 app.get('/calculator/:id1/:id2/:id3', function (req, res) {
-    let num1 = req.params.id1,
-        num2 = req.params.id2,
+    let num1 = Number(req.params.id1),
+        num2 = Number(req.params.id2),
         operator = req.params.id3;
 
     function calculator(opera) {
-        return (opera === '+') ? (num1 + num2) : (opera === '-') ? (num1 - num2) : (opera === '*') ? (num1 * num2)
-            : (opera === '/') ? (num1 / num2) : console.log('bad operator');
+
+        return (opera === '+') ? (num1 + num2) :
+            (opera === '-') ? (num1 - num2) :
+                (opera === '*') ? (num1 * num2) :
+                    (opera === 'b') ? (num1 / num2) :
+                        (opera === '%') ? (num1 % num2) : res.sendStatus(404);
     }
-    if (num1 || num2 || operator)
-        res.send('result= ' + calculator(operator));
-    else
-        res.status(400).send({ msg: 'Bad request' });
+    res.send('result= ' + calculator(operator));
 });
 
+//2nd question
 
 
-
-
-const database = ['123', '123456', '12345678', '0'];
-
-app.get('/todo', (req, res) => {
-    res.send(database);
-});
-
+var todolist = ['HTML','CSS'];
 app.post('/todo', (req, res) => {
-    database.push(req.body.todo);
-    res.send(database);
+
+    
+    var addListItem = req.body.addListItem1;
+    todolist.push(addListItem);
+    res.send('new todo= '+addListItem);
 });
 
-// app.get('/contact',function(req,res){
-//     res.send('contact me at:.....');
-// });
+app.get('/todo', function (req, res) {
 
-// app.get('/about',function(req,res){
-//     res.send('I am Erdinc');
-// })
+    res.sendfile(__dirname + '/index.html');
+    
+});
+app.get('/todos',(req,res)=>{
+
+    for (let index = 0; index < todolist.length; ++index) {
+        res.write((index + 1) + '. item=' + todolist[index]+'\n');
+    }
+    res.end();
+});
